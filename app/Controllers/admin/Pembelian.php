cd426b9d63347fa88d5fbc179bb35f9122f6b046
halimbimantara <bimadel6@gmail.com>
2020-04-22 10:51:36 +0700

    Init

---
+++ app/Controllers/admin/Pembelian.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class Pembelian extends BaseController
+{
+	public function index()
+	{
+		echo "hello";
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
